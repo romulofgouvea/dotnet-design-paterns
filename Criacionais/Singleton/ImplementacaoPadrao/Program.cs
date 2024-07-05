9121494e8cc27c6b7d88b329644599744e2ad1e4
romulo fernandes <romulo-gouvea@hotmail.com>
2024-07-05 19:20:24 -0300

    renamse Singleton

---
+++ Criacionais/Singleton/ImplementacaoPadrao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ImplementacaoPadrao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Implementação Default
+            Console.WriteLine("Instancia de s1:");
+            var s1 = Singleton.Instance;
+
+            Console.WriteLine("\nInstancia de s2:");
+            var s2 = Singleton.Instance;
+
+            Console.WriteLine($"\ns1 == s2 : {s1 == s2}");
+            #endregion
+        }
+    }
+}
